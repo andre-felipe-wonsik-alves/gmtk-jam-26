d58d96c1a7ea7df87f47c4bda1017dee5c52f45b
MarvelBR <erickmg2005@gmail.com>
2026-07-24 04:28:57 -0300

    refactor: melhorando telas e adicionando um tema padrão

--- Scripts/Screens/TransitionScreen.gd
+++ Scripts/Screens/TransitionScreen.gd
@@ -1,12 +1,13 @@
 extends Control
 
-@onready var label: RichTextLabel = $RichTextLabel
+@onready var label: RichTextLabel = $CenterContainer/VBoxContainer/RichTextLabel
+@onready var progress_bar: ProgressBar = $CenterContainer/VBoxContainer/ProgressBar
 @onready var timer: Timer = $Timer
 
 # Instruções para cada minigame (pode ser mapeado pelo índice)
 var instructions: Array[String] = [
 	"[center]Clique nos objetos [color=red]VERMELHOS![/color][/center]",
-	"Decore a sequência de cores!",
+	"[center]Decore a sequência de cores![/center]",
 ]
 
 func _ready() -> void:
@@ -14,11 +15,20 @@ func _ready() -> void:
 	if index < instructions.size():
 		label.text = instructions[index]
 	else:
-		label.text = "Prepare-se!"
-	
-	# Conectando o sinal de término do Timer via código
+		label.text = "[center]Prepare-se![/center]"
+
+	# Começa a progressbar cheia
+	progress_bar.max_value = timer.wait_time
+	progress_bar.value = timer.wait_time
+
 	timer.timeout.connect(_on_timer_timeout)
 	timer.start()
+
+func _process(_delta: float) -> void:
+	# Esvazia a barra conforme o tempo passa, dando feedback visual
+	# de quanto falta para o minigame começar
+	if not timer.is_stopped():
+		progress_bar.value = timer.time_left
 
 func _on_timer_timeout() -> void:
 	GameManager.load_current_minigame()
